355ee3f17837f168fdb7bc133a72b65eb390ce49
Николай Лучков <lnm17t088@student.bmstu.ru>
2023-07-08 20:17:59 +0300

    Добавил все модели данных. Провел их регистрацию

--- ipm_school/project/apps/main/admin.py
+++ ipm_school/project/apps/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student
+from .models import Student, TeacherPh, TeacherInf, TeacherMath, Group
 
-admin.site.register(Student)
+admin.site.register([TeacherPh, TeacherInf, TeacherMath, Group, Student])
--- ipm_school/project/apps/main/models.py
+++ ipm_school/project/apps/main/models.py
@@ -3,6 +3,55 @@ from django.db import models
 
 # Create your models here.
 
+# table Teacher of Phisic
+class TeacherPh(models.Model):
+    techer_ph_ID = models.BigAutoField(primary_key=True)
+    mail = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    faculty = models.CharField(max_length=50)
+
+
+# table Teacher of Mathematic
+class TeacherMath(models.Model):
+    techer_math_ID = models.BigAutoField(primary_key=True)
+    mail = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    faculty = models.CharField(max_length=50)
+
+
+# table Teacher of IT
+class TeacherInf(models.Model):
+    techer_inf_ID = models.BigAutoField(primary_key=True)
+    mail = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    faculty = models.CharField(max_length=50)
+
+
+# table group
+class Group(models.Model):
+    CLASS_NUM = [
+        (9, 9),
+        (10, 10),
+        (11, 11),
+    ]
+
+    class_ID = models.BigAutoField(primary_key=True)
+    class_name = models.CharField(max_length=50)
+    class_number = models.IntegerField(choices=CLASS_NUM)
+    TeacherPh_ID = models.ForeignKey(TeacherPh, null=True, on_delete=models.SET_NULL)
+    TeacherMath_ID = models.ForeignKey(TeacherMath, null=True, on_delete=models.SET_NULL)
+    TeacherInf_ID = models.ForeignKey(TeacherInf, null=True, on_delete=models.SET_NULL)
+
+
 # table of student
 class Student(models.Model):
     CLASS_NUM = [
@@ -17,6 +66,6 @@ class Student(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     class_number = models.IntegerField(choices=CLASS_NUM)
-    # class_ID = models.ForeignKey(Group, on_delete=models.SET_NULL)
+    class_ID = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
 
-#сущность учителя
+# table
